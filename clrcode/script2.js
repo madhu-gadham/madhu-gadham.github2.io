175a5a24cb2f144ec11e9690c16e00c12497faf2
g.madhu <madhuchander.g@gmail.com>
2019-07-01 21:54:16 +0530

    javascript

--- clrcode/script2.js
+++
@@ -1,9 +0,0 @@
-﻿ const bodyBcg  = document.querySelector('body');
-const colorBtn = document.querySelector('.colorBtn');
-const imge = ['yellow','red','gold','green'];
-colorBtn.addEventListener('click',getclr);
-function getclr (){
- 
-  let random = Math.floor(Math.random()*imge.length)
-bodyBcg.style.backgroundColor = imge[random];
-}
